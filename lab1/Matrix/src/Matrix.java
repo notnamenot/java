738a8496e800e2d0f49a192bc99ca532372b3e22
notnamenot <izabela.pachel@wp.pl>
2018-10-15 23:46:56 +0200

    matrix2

--- lab1/Matrix/src/Matrix.java
+++ lab1/Matrix/src/Matrix.java
@@ -5,6 +5,7 @@ public class Matrix {
     Matrix() { x = 0; y = 0; arr = new int[x][y]; }
     Matrix(int _x, int _y) { x = _x; y = _y; arr = new int[_x][_y]; }
     Matrix add(Matrix m) {
+        //if (x != m.x || y != m.y) throw new RuntimeException("Illegal vector dimensions.");
         Matrix result = new Matrix(x,y);
         for (int i = 0; i < x; ++i)
             for (int j = 0; j < y; ++j)
@@ -40,7 +41,7 @@ public class Matrix {
         n.arr[1][0] = 3;
         n.arr[1][1] = 4;
 
-        Matrix w = m.mul(n);
+        Matrix w = m.add(n);
 
         for (int i = 0; i < m.x; ++i) {
             for (int j = 0; j < m.y; ++j) {
