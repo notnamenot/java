7c7a107f6d0c3d62bbfa4d32c77a65262ee48e01
notnamenot <izabela.pachel@wp.pl>
2018-11-01 15:57:51 +0100

    correctedlab4

---
+++ lab4/Szyfrowanie/src/Algorithm.java
@@ -0,0 +1,4 @@
+interface Algorithm {
+    String crypt(String s);		//Metody w interfejsie są domyślnie publiczne i abstarkcyjne
+    String decrypt(String s);		//Pola w interfejsie domyślnie są statyczne i finalneP
+}
