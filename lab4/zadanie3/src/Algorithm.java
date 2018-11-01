c941072c1fe3f532a688148ab97bca072fc13853
notnamenot <izabela.pachel@wp.pl>
2018-11-01 15:53:11 +0100

    changelab4

--- lab4/zadanie3/src/Algorithm.java
+++
@@ -1,4 +0,0 @@
-interface Algorithm {
-    String crypt(String s);		//Metody w interfejsie są domyślnie publiczne i abstarkcyjne
-    String decrypt(String s);		//Pola w interfejsie domyślnie są statyczne i finalneP
-}
