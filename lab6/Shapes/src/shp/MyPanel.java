f2a512ff8e09c597ce32e78df0e45382e4188c78
notnamenot <izabela.pachel@wp.pl>
2018-11-13 23:28:00 +0100

    shapes vol2

--- lab6/Shapes/src/shp/MyPanel.java
+++ lab6/Shapes/src/shp/MyPanel.java
@@ -7,7 +7,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class MyPanel extends JPanel implements MouseListener {
+public class MyPanel extends JPanel implements MouseListener,MouseMotionListener {
    /* public Drawer() {
         super("Hello World");                       //konstruktor nadklasy
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //zamykanie
@@ -16,10 +16,18 @@ public class MyPanel extends JPanel implements MouseListener {
         setLocation(200,200);
     }*/
 
+    private static final int WIDTH = 200;
+    private static final int HEIGHT = 200;
+
     private int x, y;
+
+    // punkt, który będzie pamiętał współrzędne przesuwanego kwadracika
+    Point movingPoint;
 
     public MyPanel() {
         addMouseListener(this);
+        addMouseMotionListener(this);
+        //setPreferredSize(new Dimension(WIDTH, HEIGHT));
     }
 
     @Override
@@ -36,14 +44,31 @@ public class MyPanel extends JPanel implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        x = e.getX();
+        x = e.getX();       //współrzędne w których kliknięto
         y = e.getY();
+        System.out.println("tu jesten: "+x+","+y);
         //points.add(new Point(x, y));
-        repaint();
+        //repaint();
+        //int x1 = e.getX(); //współrzędne w których kliknięto
+        //int y1 = e.getY();
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        /*if (movingPoint != null) {
+            movingPoint.x = e.getX();
+            movingPoint.y = e.getY();
+            repaint();
+        }*/
+        e.getComponent().setLocation(e.getX()+e.getComponent().getX(),e.getY()+e.getComponent().getY());
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
     }
 
 
